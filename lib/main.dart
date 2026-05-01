fb726080e7e863099867375ee684c292f6849506
358elie <358elie@gmail.com>
2026-05-02 01:27:11 +0300

    Change app title from 'Local LLM' to 'Enat-AI'

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Local LLM',
+      title: 'Enat-AI',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
